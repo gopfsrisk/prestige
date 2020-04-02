fe777efec13303220fcab159b4c52967fa39cb96
gopfsrisk <63011401+gopfsrisk@users.noreply.github.com>
2020-04-02 11:43:33 -0600

    Update db_connector.py

--- prestige/db_connector.py
+++ prestige/db_connector.py
@@ -5,10 +5,12 @@ import pandas as pd
 def query_to_df(str_query):
     # establish db connection
     cnxn = pyodbc.connect("Driver={SQL Server};"
-                          "Server=electra;"
-                          "Database=pfsdb;"
-                          "Trusted_Connection=yes;")
+                      "Server=electra;"
+                      "Database=pfsdb;"
+                      "Trusted_Connection=yes;")
     # pull data into df
-    df = pd.read_sql_query(str_query, cnxn)
+    df = pd.read_sql_query(str_query, cnxn) 
     # close connection
     cnxn.close()
+    # return the df
+    return df
