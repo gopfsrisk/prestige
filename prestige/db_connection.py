f7ceb93fb89162283d421725a610cef52b9dcbdd
gopfsrisk <63011401+gopfsrisk@users.noreply.github.com>
2020-04-15 14:24:32 -0600

    Add files via upload

--- prestige/db_connection.py
+++ prestige/db_connection.py
@@ -2,12 +2,22 @@ import pyodbc
 import pandas as pd
 
 # def get query function
-def query_to_df(str_query):
-    # establish db connection
-    cnxn = pyodbc.connect("Driver={SQL Server};"
-                      "Server=electra;"
-                      "Database=pfsdb;"
-                      "Trusted_Connection=yes;")
+def query_to_df(str_query, db_engine='electra'):
+    # logic for different engines
+    if db_engine == 'electra':
+      # establish db connection
+      cnxn = pyodbc.connect("Driver={SQL Server};"
+                            "Server=electra;"
+                            "Database=pfsdb;"
+                            "Trusted_Connection=yes;")
+    elif db_engine == 'medusa':
+      # establish db connection
+      cnxn = pyodbc.connect("Driver={SQL Server};"
+                            "Server=medusa;"
+                            "Database=pfsdb;"
+                            "Trusted_Connection=yes;")
+    else:
+      print('Selected engine not found. Supported engines include "electra" and "medusa"')
     # pull data into df
     df = pd.read_sql_query(str_query, cnxn) 
     # close connection
