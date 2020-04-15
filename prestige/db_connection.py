ec8e669daec25e50b37ac49b74f48531c8da6789
gopfsrisk <63011401+gopfsrisk@users.noreply.github.com>
2020-04-15 14:50:16 -0600

    Add files via upload

--- prestige/db_connection.py
+++ prestige/db_connection.py
@@ -2,25 +2,56 @@ import pyodbc
 import pandas as pd
 
 # def get query function
-def query_to_df(str_query, db_engine='electra'):
-    # logic for different engines
-    if db_engine == 'electra':
-      # establish db connection
-      cnxn = pyodbc.connect("Driver={SQL Server};"
-                            "Server=electra;"
-                            "Database=pfsdb;"
-                            "Trusted_Connection=yes;")
-    elif db_engine == 'medusa':
-      # establish db connection
-      cnxn = pyodbc.connect("Driver={SQL Server};"
-                            "Server=medusa;"
-                            "Database=pfsdb;"
-                            "Trusted_Connection=yes;")
-    else:
-      print('ERROR! Selected engine not found. Supported engines include "electra" and "medusa"')
+def query_to_df(str_query, server='electra', database='riskdb'):
+    # define Python user-defined exceptions
+    class Error(Exception):
+      """Base class for other exceptions"""
+      pass
+
+    # error to raise if connecting to a non existent db
+    class CantFindServerAndOrDatabaseError(Error):
+      """Raised when server/databae combination not supported"""
+      pass
+
+    try:
+      # logic for different engines
+      if db_engine == 'electra' and database == 'pfsdb':
+        # establish db connection
+        cnxn = pyodbc.connect("Driver={SQL Server};"
+                              "Server=electra;"
+                              "Database=pfsdb;"
+                              "Trusted_Connection=yes;")
+        # print message
+        print('Successfully connected to electra: pfsdb.')
+      elif db_engine == 'electra' and database == 'riskdb':
+        # establish db connection
+        cnxn = pyodbc.connect("Driver={SQL Server};"
+                              "Server=electra;"
+                              "Database=riskdb;"
+                              "Trusted_Connection=yes;")
+        # print message
+        print('Successfully connected to electra: riskdb')
+      elif db_engine == 'medusa':
+        # establish db connection
+        cnxn = pyodbc.connect("Driver={SQL Server};"
+                              "Server=medusa;"
+                              "Database=pfsdb;"
+                              "Trusted_Connection=yes;")
+        # print message
+        print('Successfully connected to medusa: pfsdb')
+      else:
+        raise CantFindServerAndOrDatabaseError
+    except:
+      CantFindServerAndOrDatabaseError:
+      print('Unsupported server/database combination.')
+
     # pull data into df
     df = pd.read_sql_query(str_query, cnxn) 
+    # print message
+    print(f'Successfully pulled query from {server}: {database} and stored in dataframe.')
     # close connection
     cnxn.close()
+    # print message
+    print('Connection closed.')
     # return the df
     return df
