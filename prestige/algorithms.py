34bb15c92e3ac0085fee563677ab2ae9b73f46d2
Aaron England <aengland@gopfs.com>
2020-05-18 16:10:16 -0600

    added train test split dependency

--- prestige/algorithms.py
+++ prestige/algorithms.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from numpy.linalg import inv
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 # linear regression class
 class OLSRegression:
