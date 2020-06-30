7fe8a0864d5f6675df3050c5c3caf5e4c93e51f4
Aaron England <aengland@gopfs.com>
2020-06-30 13:57:28 -0600

    binary classification eval metrics function

--- prestige/model_eval.py
+++ prestige/model_eval.py
@@ -35,3 +35,40 @@ def roc_auc_curve(y_true, y_hat, tpl_figsize=(10,10)):
 	# return fig
 	return fig
 
+# define function to get binary eval metrics
+def bin_class_eval_metrics(model_classifier, X, y):
+	# generate predicted class
+	y_hat_class = model_classifier.predict(X)
+	# generate predicted probabilities
+	y_hat_proba = model_classifier.predict_proba(X)[:,1]
+	# metrics
+	# accuracy
+	accuracy = accuracy_score(y_true=y, y_pred=y_hat_class)
+	# geometric mean
+	geometric_mean = fowlkes_mallows_score(labels_true=y, labels_pred=y_hat_class)
+	# precision
+	precision = precision_score(y_true=y, y_pred=y_hat_class)
+	# recall
+	recall = recall_score(y_true=y, y_pred=y_hat_class)
+	# f1
+	f1 = f1_score(y_true=y, y_pred=y_hat_class)
+	# roc auc
+	roc_auc = roc_auc_score(y_true=y, y_score=y_hat_proba)
+	# precision recall auc
+	pr_auc = average_precision_score(y_true=y, y_score=y_hat_proba)
+	# log loss
+	log_loss_ = log_loss(y_true=y, y_pred=y_hat_proba)
+	# brier 
+	brier = brier_score_loss(y_true=y, y_prob=y_hat_proba)
+	# put into dictionary
+	dict_ = {'accuracy': accuracy,
+	         'geometric_mean': geometric_mean,
+	         'precision': precision,
+	         'recall': recall,
+	         'f1': f1,
+	         'roc_auc': roc_auc,
+	         'pr_auc': pr_auc,
+	         'log_loss': log_loss_,
+	         'brier': brier}
+	# return dict_
+	return dict_
